fe1b20b43afa5e4c030cfc81493d0536b2ac1cbb
ma <rv.nirmalala@gmail.com>
2022-04-28 19:04:35 +0300

    Adding Recent work

--- features/home/components/Header.tsx
+++ features/home/components/Header.tsx
@@ -13,25 +13,29 @@ import {
   Heading,
   Collapse,
 } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  HamburgerIcon, CloseIcon, MoonIcon, SunIcon,
+} from '@chakra-ui/icons';
 import style from '../../../styles/Headers.module.css';
 
 const Links = ['Home', 'Blog'];
 
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}
-  >
-    {children}
-  </Link>
-);
+function NavLink({ children }: { children: ReactNode }) {
+  return (
+    <Link
+      px={2}
+      py={1}
+      rounded="md"
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href="/"
+    >
+      {children}
+    </Link>
+  );
+}
 
 export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -39,26 +43,36 @@ export default function Header() {
 
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-      <Flex maxW={'2xl'} h={16} alignItems={'center'} justifyContent={'space-between'} margin="auto">
+      <Flex
+        maxW="2xl"
+        h={16}
+        alignItems="center"
+        justifyContent="space-between"
+        margin="auto"
+      >
         <IconButton
-          size={'md'}
+          size="md"
           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={'Open Menu'}
+          aria-label="Open Menu"
           display={{ md: 'none' }}
           onClick={isOpen ? onClose : onOpen}
         />
-        <HStack spacing={8} alignItems={'center'}>
+        <HStack spacing={8} alignItems="center">
           <Box>
-            <Link className={style.linkReset}>
-              <Heading size='sm' cursor='pointer'>
-                {'<'}Ma <div className={style.emoji} data-hover='👋'></div>😎/
+            <Link className={style.linkReset} href="/">
+              <Heading size="sm" cursor="pointer">
+                {'<'}
+                Ma
+                {' '}
+                <div className={style.emoji} data-hover="👋" />
+                😎/
                 {'>'}
               </Heading>
             </Link>
           </Box>
         </HStack>
-        <Flex alignItems={'center'}>
-          <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+        <Flex alignItems="center">
+          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
             {Links.map((link) => (
               <NavLink key={link}>{link}</NavLink>
             ))}
@@ -72,7 +86,7 @@ export default function Header() {
       <Collapse in={isOpen} animateOpacity>
         {isOpen ? (
           <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
+            <Stack as="nav" spacing={4}>
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
--- features/index.ts
+++ features/index.ts
@@ -1,3 +1,4 @@
 import HomePage from './home/page/HomePage';
 
+// eslint-disable-next-line import/prefer-default-export
 export { HomePage };
